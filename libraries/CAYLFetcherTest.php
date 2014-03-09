d2ba8970298a0abd52ca875649b6680191b64c02
Jeffrey Licht <jeffrey.licht@gmail.com>
2014-03-09 12:20:58 -0400

    Fix failing placeholder tests for PHPUnit

--- libraries/CAYLFetcherTest.php
+++ libraries/CAYLFetcherTest.php
@@ -11,7 +11,7 @@ class CAYLRobotsTest extends \PHPUnit_Framework_TestCase {
 
   public function testRobotsParse()
   {
-    $this->assertFalse(CAYLRobots::url_permitted(NULL,"www.google.com"));
+    $this->assertTrue(CAYLRobots::url_permitted(NULL,"www.google.com"));
     $this->assertTrue(CAYLRobots::url_permitted("Donuts","www.google.com"));
   }
 
