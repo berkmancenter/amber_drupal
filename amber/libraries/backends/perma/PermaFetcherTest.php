f6e499f861f9ebf9338b6497625ef401d8e9f0dd
Jeffrey Licht <jlicht@podconsulting.com>
2015-08-03 21:53:24 -0400

    Fix fetcher tests

--- amber/libraries/backends/perma/PermaFetcherTest.php
+++ amber/libraries/backends/perma/PermaFetcherTest.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("PermaFetcher.php");
+require_once("PermaStorage.php");
 
 class PermaAmberFetcherTest extends \PHPUnit_Framework_TestCase {
 
@@ -14,7 +15,7 @@ class PermaAmberFetcherTest extends \PHPUnit_Framework_TestCase {
      */
 	public function testBadConfiguration()
 	{
-		$fetcher = new PermaFetcher(array());
+		$fetcher = new PermaFetcher(new PermaStorage(array()), array());
 		$result = $fetcher->fetch("http://www.google.com");
 	}
 
@@ -24,37 +25,24 @@ class PermaAmberFetcherTest extends \PHPUnit_Framework_TestCase {
      */
 	public function testMissingURL()
 	{
-		$fetcher = new PermaFetcher(array());
+		$fetcher = new PermaFetcher(new PermaStorage(array()), array());
 		$result = $fetcher->fetch("");
 	}
 
     /**
-     * @expectedException        PHPUnit_Framework_Error
+     * @expectedException        RuntimeException
      */
 	public function testBadAPIKeyTriggersPHPError()
 	{
-		$fetcher = new PermaFetcher(array('perma_api_key' => "bogus"));
+		$fetcher = new PermaFetcher(new PermaStorage(array()), array('perma_api_key' => "bogus"));
 		$result = $fetcher->fetch("http://www.google.com");
-	}
-
-	public function testBadAPIKeyReturnsFalse()
-	{
-		PHPUnit_Framework_Error_Warning::$enabled = FALSE;
-		$default_error_logging_level = error_reporting();
-	    error_reporting(E_ALL ^ E_WARNING);
-
-		$fetcher = new PermaFetcher(array('perma_api_key' => "bogus"));	  
-		$result = $fetcher->fetch("http://www.google.com");
-		$this->assertFalse($result);
-
-	    error_reporting($default_error_logging_level);
 	}
 
 	public function testGoodAPIKeyReturnsExpectedResponse()
 	{
 		$apikey = getenv("PERMA_API_KEY");
 		if ($apikey) {			
-			$fetcher = new PermaFetcher(array('perma_api_key' => $apikey));	  
+			$fetcher = new PermaFetcher(new PermaStorage(array()), array('perma_api_key' => $apikey));	  
 			$result = $fetcher->fetch("http://www.google.com");
 			$this->assertEquals($result['url'],"http://www.google.com");
 			$this->assertNotEmpty($result['id']);
