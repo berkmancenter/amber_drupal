237cf501855f5ed82ccc30d039be67c400ea2887
Jeffrey Licht <jlicht@podconsulting.com>
2015-09-09 09:37:29 -0400

    Fix test error

--- amber/libraries/AmberAvailabilityLookupTest.php
+++ amber/libraries/AmberAvailabilityLookupTest.php
@@ -1,7 +1,7 @@
 <?php
 require_once ("AmberNetworkUtils.php");
 require_once ("AmberAvailabilityLookup.php");
-class AmberCheckerTest extends PHPUnit_Framework_TestCase {
+class AmberNetClerkAvailabilityLookupTest extends PHPUnit_Framework_TestCase {
 
   protected function setUp() {
     date_default_timezone_set('UTC');
